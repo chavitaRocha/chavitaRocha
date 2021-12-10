97eb478807da8ba3a0f764b56c1b920171eaa253
Chavita1386 <chavita1386@gmail.com>
2021-12-10 10:33:05 -0600

    style: add experience styles for mobile and desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
   ],
   darkMode: "media", // or 'media' or 'class'
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
         primary: "#009DAE",
